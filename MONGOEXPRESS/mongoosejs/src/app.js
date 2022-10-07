fa10f5857249caf39e0a76eb28d58ead7f1c586c
saurabh1024 <saurabhambesange0@gmail.com>
2022-10-08 01:56:20 +0530

    refactoring

--- MONGOEXPRESS/mongoosejs/src/app.js
+++ MONGOEXPRESS/mongoosejs/src/app.js
@@ -213,4 +213,61 @@ const updateDoc = async () => {
   catch (err) { console.log(err) }
 }
 
-  updateDoc();
+updateDoc();
+  
+
+
+
+
+
+// const mongoose = require("mongoose");
+// mongoose
+//   .connect("mongodb://localhost:27017/students-database", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   })
+//   .then(() => {
+//     console.log("database connection established");
+//   })
+//   .catch((e) => {
+//     console.log("db connection failed");
+//   });
+
+
+
+// const mongoose = require("mongoose");
+// const validator = require("validator");
+// const { default: isEmail } = require("validator/lib/isemail");
+
+// const studentSchema = new mongoose.Schema({  // creating a new schema for the student database which defines what type of data is acceptable //
+//   name: {
+//     type: String,
+//     required: true,
+//     minlength: 3,
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: [true, "Email id already exists"],
+//     validate(value) {
+//       if (!validator.isEmail(value)) {
+//         throw new Error("Invalid Email Id");
+//       }
+//     },
+//   },
+//   phone: {
+//     type: Number,
+//     required: true,
+//     min: 10,
+//     unique: true,
+//   },
+//   address: {
+//     type: String,
+//     required: true,
+//   },
+// });
+
+
+// const Student = new mongoose.model("Student", studentSchema)  // creating a new collection named student( shud be singular & shud follow pascal convention) 
+
+// module.exports = Student // exporting student module to express app
